711c6904302f171f9eb99164b2ae31a5817b3821
pmarchezz <pmarchezz@users.noreply.github.com>
2015-06-05 17:08:47 +0200

    Enabled pushstate

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import 'bootstrap/css/bootstrap.css!';
 export class App {
   configureRouter(config, router){
     config.title = 'Aurelia';
+    config.options.pushState = true;
     config.map([
       { route: ['','welcome'],  moduleId: './welcome',      nav: true, title:'Welcome' },
       { route: 'flickr',        moduleId: './flickr',       nav: true, title:'Flickr' },
